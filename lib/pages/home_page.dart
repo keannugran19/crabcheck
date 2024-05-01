17c3995f3aee2309497ad650f3f08ec5c625e8ca
keannugran19 <main.grankeannu@gmail.com>
2024-05-01 11:37:57 +0800

    capture and upload working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:crabcheck/constants/colors.dart';
+import 'package:crabcheck/components/home_button.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  set image(File image) {}
 
   @override
   Widget build(BuildContext context) {
     const appName = "CRABCHECK";
+
+    // Camera and Image Integration
+    File? image;
+    Future pickImage() async {
+      try {
+        final image = await ImagePicker().pickImage(source: ImageSource.camera);
+        if (image == null) return;
+        final imageTemp = File(image.path);
+        setState(() => this.image = imageTemp);
+      } on PlatformException catch (e) {
+        print('Failed to pick image: $e');
+      }
+    }
 
     return Scaffold(
       body: SafeArea(
@@ -44,7 +69,31 @@ class HomePage extends StatelessWidget {
                       fontSize: 35),
                 )
               ],
-            )
+            ),
+
+            const SizedBox(
+              height: 60,
+            ),
+
+            // Capture button
+            HomeButton(
+              buttonText: "Capture",
+              buttonColor: colorScheme.secondary,
+              onPressed: () {
+                pickImage();
+              },
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            HomeButton(
+                buttonText: "Upload",
+                buttonColor: colorScheme.secondary,
+                onPressed: () {
+                  pickImage();
+                }),
           ]),
         ),
       ),
